c53d0003ce31c9fdd86400c4fb490322280059ee
Charuka Samarakoon <charuka@protonmail.com>
2024-01-08 00:45:19 +0530

    fix: local file import fail

--- goose_pb/pydantic/search_mod_request_p2p.py
+++ goose_pb/pydantic/search_mod_request_p2p.py
@@ -2,7 +2,7 @@
 # gen by protobuf_to_pydantic[v0.2.3](https://github.com/so1n/protobuf_to_pydantic)
 # Protobuf Version: 4.25.1 
 # Pydantic Version: 2.5.3 
-from email_p2p import EmailDocument
+from .email_p2p import EmailDocument
 from google.protobuf.message import Message  # type: ignore
 from pydantic import BaseModel
 from pydantic import Field
